5b0bb5a2d44bc13c01680d7fc31adf0dcc5c2622
Gourab Nag <gourabnag12@gmail.com>
2020-12-24 20:27:17 +0530

    init

---
+++ script.js
@@ -0,0 +1,10 @@
+// const camera = document.querySelector("#camera");
+// let zoom = 1;
+// function zoomFn(event) {
+//   console.log(event);
+//   zoom += event.deltaY * -0.05;
+
+//   camera.object3D.scale.x = zoom;
+// }
+
+// document.body.onwheel = zoomFn;
